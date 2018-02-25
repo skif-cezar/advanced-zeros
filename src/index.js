c8707c0e255ed4c81498e491deffe71138babcd5
skif-cezar <33913952+skif-cezar@users.noreply.github.com>
2018-02-25 20:01:05 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+var arr = [];
+var f = 2;
+while(f <= base){
+    if(base%f == 0){
+        arr = base;
+        base = Math.round(base/f);
+    }else {
+        f++;
+    }
+}
+var res = 0;
+for (var i = 1; i < 12; i++){
+    var num = Math.floor(number/Math.pow(arr, i))
+    res += num;
+}
+return res;
 }
